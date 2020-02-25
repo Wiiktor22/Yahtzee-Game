15cf3e369903184baf59957cf69f26078fc6b259
Wiktor Szlegier <22wiktors@gmail.com>
2020-02-25 15:24:16 +0100

    Updates

---
+++ precache-manifest.2d1475fa6fd50a06da2dcccb3a0349d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb608e7f3d7e4e0f78d35ab8630698fb",
+    "url": "/Yahtzee-Game/index.html"
+  },
+  {
+    "revision": "67297ec260d2e485f059",
+    "url": "/Yahtzee-Game/static/css/main.d7fa7af1.chunk.css"
+  },
+  {
+    "revision": "6978fcdea8a4a3110d08",
+    "url": "/Yahtzee-Game/static/js/2.d49fcef9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Yahtzee-Game/static/js/2.d49fcef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67297ec260d2e485f059",
+    "url": "/Yahtzee-Game/static/js/main.d852ec17.chunk.js"
+  },
+  {
+    "revision": "44109e802e2cf065cdda",
+    "url": "/Yahtzee-Game/static/js/runtime-main.f59c974c.js"
+  }
+]);
